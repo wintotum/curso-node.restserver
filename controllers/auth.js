25427b19445337b4980468f74838ebd8afe5086f
Ignacio Febles <ifebles@ipsvial.es>
2021-03-01 20:06:34 +0100

    Google sing added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async ( req, res = response ) => {
@@ -53,6 +54,58 @@ const login = async ( req, res = response ) => {
 }
 
 
+const googleSigin = async( req, res = response) => {
+
+    const { id_token } = req.body;
+
+    
+    try {
+        
+        const { correo, nombre, img } = await googleVerify( id_token );
+    
+        // console.log('usuario de Google->', googleUser);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            //Tengo que crearlo
+            const data = { 
+                nombre,
+                correo,
+                password: 'P:',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        //Generar el jwt
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token            
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+        
+    }
+
+}
+
+
 module.exports = {
-    login
+    login, 
+    googleSigin
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { check } = require('express-validator');
 const {  Router } = require('express');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { login } = require('../controllers/auth');
+const { login, googleSigin } = require('../controllers/auth');
 
 const router = Router();
 
@@ -11,5 +11,9 @@ router.post('/login', [
     validarCampos
 ], login );
 
+router.post('/google', [
+    check('id_token','El id_token es necesario').not().isEmpty(),
+    validarCampos
+],  googleSigin );
 
 module.exports = router;
